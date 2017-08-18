b1a5b84c4d49e89cce8dd0343ffacc2fa5c86464
zhangxiao <243040703>
2017-08-18 14:38:00 +0800

    del

--- app/static/js/nav.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-
-	$('.nav_list .nav_item').on('click','a',function(){
-		$(this).parent().addClass('active').siblings().removeClass('active');
-		$('.nav_none>ul:eq('+$(this).parent().index()+')').show().siblings().hide();	
-		return false;
-	})
-}) 
